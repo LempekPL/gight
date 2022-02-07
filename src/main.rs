b4bea4ed8cd1db766ce9b40f869878fcba21989e
LempekPL <superlempek2004@gmail.com>
2022-02-07 23:31:12 +0100

    change to bevy + first main menu

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,54 @@
-extern crate sdl2;
+mod main_menu;
+
+use bevy::prelude::*;
+use bevy_asset_loader::{AssetLoader, AssetCollection};
+
+#[derive(Clone, Eq, PartialEq, Debug, Hash)]
+enum GameState {
+    LoadingAssets,
+    Loading,
+    MainMenu,
+}
+
+#[derive(AssetCollection)]
+struct FontAssets {
+    #[asset(path = "fonts/open_sans/OpenSans-Bold.ttf")]
+    open_sans_regular: Handle<Font>,
+}
 
 fn main() {
-    let _sdl = sdl2::init().unwrap();
+    let mut app = App::new();
+    AssetLoader::new(GameState::LoadingAssets)
+        .continue_to_state(GameState::Loading)
+        .with_collection::<FontAssets>()
+        .build(&mut app);
+    app.insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)));
+    app.insert_resource(WindowDescriptor {
+        title: "Gight".to_string(),
+        width: (1280.0 / 2.0),
+        height: (960.0 / 2.0),
+        resizable: false,
+        ..Default::default()
+    });
+    // this will tell the asset loader to load assets after loading assets it will change state loding to initiate setup
+    app.add_state(GameState::LoadingAssets);
+    app.add_plugins(DefaultPlugins);
+    // main menu systems
+    app.add_plugin(main_menu::MainMenuPlugin);
+    // initiating setup
+    app.add_system_set(SystemSet::on_enter(GameState::Loading).with_system(setup));
+
+
+    app.run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut app_state: ResMut<State<GameState>>,
+) {
+    // camera 2d
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    // camera ui
+    commands.spawn_bundle(UiCameraBundle::default());
+    app_state.set(GameState::MainMenu).unwrap();
 }
---
+++ src/main_menu.rs
@@ -0,0 +1,75 @@
+use bevy::prelude::*;
+use crate::{FontAssets, GameState};
+
+pub struct MainMenuPlugin;
+
+#[derive(Component)]
+struct MainMenuButton;
+
+#[derive(Component)]
+struct MainMenuText;
+
+impl Plugin for MainMenuPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .add_system_set(SystemSet::on_enter(GameState::MainMenu).with_system(spawn_main_menu))
+            .add_system_set(SystemSet::on_update(GameState::MainMenu).with_system(handle_main_menu));
+    }
+}
+
+fn spawn_main_menu(
+    mut commands: Commands,
+    font_assets: Res<FontAssets>,
+) {
+    commands
+        .spawn_bundle(ButtonBundle {
+            style: Style {
+                size: Size::new(Val::Px(150.0), Val::Px(65.0)),
+                // center button
+                margin: Rect::all(Val::Auto),
+                // horizontally center child text
+                justify_content: JustifyContent::Center,
+                // vertically center child text
+                align_items: AlignItems::Center,
+                ..Default::default()
+            },
+            color: Color::WHITE.into(),
+            ..Default::default()
+        })
+        .with_children(|parent| {
+            parent.spawn_bundle(TextBundle {
+                text: Text::with_section(
+                    "PLAY",
+                    TextStyle {
+                        font: font_assets.open_sans_regular.clone(),
+                        font_size: 40.0,
+                        color: Color::WHITE,
+                    },
+                    Default::default(),
+                ),
+                ..Default::default()
+            }).insert(MainMenuText);
+        })
+        .insert(MainMenuButton);
+}
+
+fn handle_main_menu(
+    mut interaction_query: Query<
+        (&Interaction, &mut UiColor),
+        (Changed<Interaction>, With<MainMenuButton>),
+    >,
+) {
+    for (interaction, mut color) in interaction_query.iter_mut() {
+        match *interaction {
+            Interaction::Clicked => {
+                *color = Color::YELLOW.into();
+            }
+            Interaction::Hovered => {
+                *color = Color::GREEN.into();
+            }
+            Interaction::None => {
+                *color = Color::WHITE.into();
+            }
+        }
+    }
+}
