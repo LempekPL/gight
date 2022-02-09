40803402fe5df0968131db13b2631b572611782c
LempekPL <superlempek2004@gmail.com>
2022-02-09 01:44:14 +0100

    loading text and vertically aligned buttons (Quit works)

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ use bevy_asset_loader::{AssetLoader, AssetCollection};
 use bevy_kira_audio::{AudioPlugin, AudioSource};
 
 #[derive(Clone, Eq, PartialEq, Debug, Hash)]
-enum GameState {
+enum AppState {
+    Preload,
     LoadingAssets,
     Loading,
     MainMenu,
@@ -19,14 +20,19 @@ struct FontAssets {
 
 #[derive(AssetCollection)]
 struct SoundAssets {
+    // sloooow loading,
+    // when creator of bevy_asset_loader will make compatibility with bevy_loading then you could at least look at progress bar, now just stare at the loading text :/
     #[asset(path = "sounds/main_menu_song.ogg")]
     main_menu: Handle<AudioSource>,
 }
 
+#[derive(Component)]
+struct LoadingText;
+
 fn main() {
     let mut app = App::new();
-    AssetLoader::new(GameState::LoadingAssets)
-        .continue_to_state(GameState::Loading)
+    AssetLoader::new(AppState::LoadingAssets)
+        .continue_to_state(AppState::Loading)
         .with_collection::<FontAssets>()
         .with_collection::<SoundAssets>()
         .build(&mut app);
@@ -38,26 +44,73 @@ fn main() {
         resizable: false,
         ..Default::default()
     });
+    app.add_startup_system(preload);
     // this will tell the asset loader to load assets after loading assets it will change state loding to initiate setup
-    app.add_state(GameState::LoadingAssets);
+    app.add_state(AppState::Preload);
     app.add_plugins(DefaultPlugins);
     app.add_plugin(AudioPlugin);
     // main menu systems
     app.add_plugin(main_menu::MainMenuPlugin);
     // initiating setup
-    app.add_system_set(SystemSet::on_enter(GameState::Loading).with_system(setup));
+    app.add_system_set(SystemSet::on_enter(AppState::Loading).with_system(setup));
 
 
     app.run();
 }
 
-fn setup(
+fn preload(
     mut commands: Commands,
-    mut app_state: ResMut<State<GameState>>,
+    mut app_state: ResMut<State<AppState>>,
+    asset_server: Res<AssetServer>,
 ) {
-    // camera 2d
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     // camera ui
     commands.spawn_bundle(UiCameraBundle::default());
-    app_state.set(GameState::MainMenu).unwrap();
+
+    // TODO: make some animation so it looks like something is actually doing
+    // loading text so people would know that it's working
+    commands.spawn_bundle(NodeBundle {
+        style: Style {
+            size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+            position_type: PositionType::Absolute,
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::FlexEnd,
+            ..Default::default()
+        },
+        color: UiColor(Color::BLACK),
+        ..Default::default()
+    }).with_children(|parent| {
+        parent.spawn_bundle(TextBundle {
+            style: Style {
+                align_self: AlignSelf::Center,
+                ..Default::default()
+            },
+            text: Text::with_section(
+                "LOADING",
+                TextStyle {
+                    font: asset_server.load("fonts/open_sans/OpenSans-Bold.ttf"),
+                    font_size: 80.0,
+                    color: Color::WHITE,
+                },
+                Default::default()
+            ),
+            ..Default::default()
+        });
+    }).insert(LoadingText);
+    // start loading assets
+    app_state.set(AppState::LoadingAssets).unwrap();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut app_state: ResMut<State<AppState>>,
+    loading_text_query: Query<Entity, With<LoadingText>>
+) {
+    // remove loading text
+    let loading_text_entity = loading_text_query.single();
+    commands.entity(loading_text_entity).despawn_recursive();
+
+    // camera 2d
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    // move user to main menu
+    app_state.set(AppState::MainMenu).unwrap();
 }
